5e54829add8fe777064158698bb3211247d10f1b
Wellington <wellington.esteba.romero@gmail.com>
2025-02-17 17:15:36 +0100

    bootstrap.min.css mejor validacion formulario

--- webapp-form/src/main/java/com/webapp/form/servlet/FormServlet.java
+++ webapp-form/src/main/java/com/webapp/form/servlet/FormServlet.java
@@ -8,9 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @WebServlet("/form")
 public class FormServlet extends HttpServlet {
@@ -26,48 +24,47 @@ public class FormServlet extends HttpServlet {
         String[] lenguajes = req.getParameterValues("lenguajes");
         String[] roles = req.getParameterValues("roles");
         String idioma = req.getParameter("idioma");
-        boolean habilitar = req.getParameter("habilitar") != null &&
-                "on".equals(req.getParameter("habilitar"));
+        boolean habilitar = req.getParameter("habilitar") != null && "on".equals(req.getParameter("habilitar"));
         String secret = req.getParameter("secret");
 
-        List<String> errores = new ArrayList<String>();
+        Map<String,String> errores = new HashMap<>();
 
         if (username == null || username.isBlank()) {
-            errores.add("El usuario no puede estar vacio");
+            errores.put("username", "El usuario no puede estar vacío");
         }
         if (password == null || password.isBlank()) {
-            errores.add("El password no puede estar vacio");
+            errores.put("password", "El password no puede estar vacío");
         }
-        if (email == null || email.isBlank() ) {
-            errores.add("El email no puede estar vacio");
+        if (email == null || email.isBlank()) {
+            errores.put("email", "El email no puede estar vacío");
         } else if (!email.contains("@")) {
-            errores.add("El email debe tener un formato válido");
+            errores.put("email", "El email debe tener un formato válido");
         }
         if (pais == null || pais.isBlank()) {
-            errores.add("El pais no puede estar vacio");
+            errores.put("pais", "El pais no puede estar vacío");
         }
         if (lenguajes == null || lenguajes.length == 0) {
-            errores.add("Debe seleccionar un lenguaje");
+            errores.put("lenguajes", "Debe seleccionar un lenguaje");
         }
         if (roles == null || roles.length == 0) {
-            errores.add("Debe seleccionar un role");
+            errores.put("roles", "Debe seleccionar un role");
         }
         if (idioma == null) {
-            errores.add("El idioma no puede estar vacio");
+            errores.put("idioma", "El idioma no puede estar vacío");
         }
 
-        try (PrintWriter out = resp.getWriter()) {
+        if (errores.isEmpty()) {
+            try (PrintWriter out = resp.getWriter()) {
 
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("   <head>");
-            out.println("       <meta charset=\"UTF-8\">");
-            out.println("       <title>Resultado Form</title>");
-            out.println("   </head>");
-            out.println("   <body>");
-            out.println("       <h1>Resultado Form</h1>");
-            out.println("   <ul>");
-            if (errores.isEmpty()) {
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("   <head>");
+                out.println("       <meta charset=\"UTF-8\">");
+                out.println("       <title>Resultado Form</title>");
+                out.println("   </head>");
+                out.println("   <body>");
+                out.println("       <h1>Resultado Form</h1>");
+                out.println("   <ul>");
                 out.println("       <li>Usename: " + username + "</li>");
                 out.println("       <li>Password: " + password + "</li>");
                 out.println("       <li>Email: " + email + "</li>");
@@ -85,15 +82,17 @@ public class FormServlet extends HttpServlet {
                 out.println("       <li>Idioma: " + idioma + "</li>");
                 out.println("       <li>Habilitar: " + habilitar + "</li>");
                 out.println("       <li>Secreto: " + secret + "</li>");
-            } else {
-                errores.forEach(error -> {
-                    out.println("       <li>" + error + "</li>");
-                });
-                out.println("<p><a href=\"/webapp-form/index.html\">Volver</a></p>");
+                out.println("   </ul>");
+                out.println("   </body>");
+                out.println("</html>");
             }
-            out.println("   </ul>");
-            out.println("   </body>");
-            out.println("</html>");
+        } else {
+//                errores.forEach(error -> {
+//                    out.println("       <li>" + error + "</li>");
+//                });
+//                out.println("<p><a href=\"/webapp-form/index.jsp\">Volver</a></
+            req.setAttribute("errores", errores);
+            getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
         }
     }
 }
